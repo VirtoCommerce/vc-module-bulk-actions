742f235d3e6f84f9a2d09b64132b13ebb8b122a0
Timofey <tburashov@gmail.com>
2019-10-11 12:48:35 +0700

    remove useless scripts of getting the resources from API

--- VirtoCommerce.BulkActionsModule/VirtoCommerce.BulkActionsModule.Web/Scripts/resources/virto-commerce.catalog-bulk-actions-module-api.js
+++
@@ -1,5 +0,0 @@
-angular.module('virtoCommerce.bulkActionsModule')
-    .factory('virtoCommerce.bulkActionsModule.webApi', ['$resource', function ($resource) {
-        return $resource('', null, {
-        });
-}]);
