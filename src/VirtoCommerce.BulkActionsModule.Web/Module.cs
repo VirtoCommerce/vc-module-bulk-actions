d56472cfbc49be6a6ac275dcd5f74ba5d9f14467
Andrei Kolchanov <akak1977@users.noreply.github.com>
2021-04-09 13:59:19 +0500

    PT-989: Remove obsolete BulkActionContextJsonConverter (#9)

--- src/VirtoCommerce.BulkActionsModule.Web/Module.cs
+++ src/VirtoCommerce.BulkActionsModule.Web/Module.cs
@@ -1,13 +1,12 @@
 using System.Linq;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using VirtoCommerce.BulkActionsModule.Core;
+using VirtoCommerce.BulkActionsModule.Core.Models.BulkActions;
 using VirtoCommerce.BulkActionsModule.Core.Services;
 using VirtoCommerce.BulkActionsModule.Data.Services;
 using VirtoCommerce.BulkActionsModule.Web.BackgroundJobs;
-using VirtoCommerce.BulkActionsModule.Web.JsonConverters;
+using VirtoCommerce.Platform.Core.JsonConverters;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Security;
 
@@ -29,8 +28,7 @@ namespace VirtoCommerce.BulkActionsModule.Web
             var permissionsProvider = appBuilder.ApplicationServices.GetRequiredService<IPermissionsRegistrar>();
             permissionsProvider.RegisterPermissions(ModuleConstants.Security.Permissions.AllPermissions.Select(x => new Permission() { GroupName = "BulkActions", Name = x }).ToArray());
 
-            var mvcJsonOptions = appBuilder.ApplicationServices.GetService<IOptions<MvcNewtonsoftJsonOptions>>();
-            mvcJsonOptions.Value.SerializerSettings.Converters.Add(new BulkActionContextJsonConverter());
+            PolymorphJsonConverter.RegisterTypeForDiscriminator(typeof(BulkActionContext), nameof(BulkActionContext.ContextTypeName));
         }
 
         public void Uninstall()
