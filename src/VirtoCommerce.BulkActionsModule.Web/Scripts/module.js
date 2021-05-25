67f25e0895ffcf331df692622e9ef02a4f9d409c
Egis <em@virtoway.com>
2021-05-25 13:01:42 +0300

    PT-1809: Add custom push notification header template (#14)

--- src/VirtoCommerce.BulkActionsModule.Web/Scripts/module.js
+++ src/VirtoCommerce.BulkActionsModule.Web/Scripts/module.js
@@ -6,51 +6,15 @@ if (AppDependencies !== undefined) {
 }
 
 angular.module(moduleName, [])
-    .config(['$stateProvider', '$urlRouterProvider',
-        function ($stateProvider, $urlRouterProvider) {
-            $stateProvider
-                .state('workspace.virtoCommerceBulkActionsModuleState', {
-                    url: '/virtoCommerce.bulkActionsModule',
-                    templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
-                    controller: [
-                        '$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
-                            var newBlade = {
-                                id: 'blade1',
-                                controller: 'virtoCommerce.bulkActionsModule.helloWorldController',
-                                template: 'Modules/$(virtoCommerce.bulkActionsModule)/Scripts/blades/hello-world.html',
-                                isClosingDisabled: true
-                            };
-                            bladeNavigationService.showBlade(newBlade);
-                        }
-                    ]
-                });
-        }
-    ])
-    .run([
-        '$rootScope',
-        'platformWebApp.mainMenuService',
-        'platformWebApp.widgetService',
-        '$state', 'platformWebApp.toolbarService',
-        'platformWebApp.bladeUtils',
-        'platformWebApp.bladeNavigationService',
-        function ($rootScope,
-            mainMenuService,
-            widgetService,
-            $state,
-            toolbarService,
-            bladeUtils,
-            bladeNavigationService) {
+    .run(['platformWebApp.pushNotificationTemplateResolver', '$state', 'platformWebApp.bladeNavigationService',
+        function (pushNotificationTemplateResolver, $state, bladeNavigationService) {
 
-            //Register module in main menu
-            //var menuItem = {
-            //    path: 'browse/virtoCommerce.bulkActionsModule',
-            //    icon: 'fa fa-cube',
-            //    title: 'VirtoCommerce.bulkActionsModule',
-            //    priority: 100,
-            //    action: function () { $state.go('workspace.virtoCommerceBulkActionsModuleState'); },
-            //    permission: 'virtoCommerce.bulkActionsModule.WebPermission'
-            //};
-            //mainMenuService.addMenuItem(menuItem);
-
+            // register notification template
+            pushNotificationTemplateResolver.register({
+                priority: 900,
+                satisfy: (notify, place) => place === 'header-notification' && notify.notifyType === 'BulkActionPushNotification',
+                template: 'Modules/$(VirtoCommerce.BulkActionsModule)/Scripts/notifications/headerNotification.tpl.html',
+                action: function (notify) { $state.go('workspace.pushNotificationsHistory', notify) }
+            });
         }
-]);
+    ]);
