9de71b14be9063d991850833b4afe1e38b02b1fd
Filipp Kuksov <47894313+Vectorfield4@users.noreply.github.com>
2021-05-25 10:20:30 +0200

    Revert "PT-777: Remove redundant class and update dependencies (#13)"
    This reverts commit 42237ab7337dc6f15c697b941ec2cd32024d6837.

---
+++ src/VirtoCommerce.BulkActionsModule.Web/BackgroundJobs/JobCancellationTokenWrapper.cs
@@ -0,0 +1,27 @@
+﻿namespace VirtoCommerce.BulkActionsModule.Web.BackgroundJobs
+{
+    using Hangfire;
+
+    using VirtoCommerce.Platform.Core.Common;
+
+    public class JobCancellationTokenWrapper : ICancellationToken
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobCancellationTokenWrapper"/> class.
+        /// </summary>
+        /// <param name="jobCancellationToken">
+        /// The job cancellation token.
+        /// </param>
+        public JobCancellationTokenWrapper(IJobCancellationToken jobCancellationToken)
+        {
+            JobCancellationToken = jobCancellationToken;
+        }
+
+        public IJobCancellationToken JobCancellationToken { get; }
+
+        public void ThrowIfCancellationRequested()
+        {
+            JobCancellationToken.ThrowIfCancellationRequested();
+        }
+    }
+}
